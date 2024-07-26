2447810ade89baf433a6e5c3b008c2632fe1a299
Danielle Hunter <danielle.hunter@secturasoft.com>
2024-07-25 22:50:59 -0500

    error handling

--- src/pages/api/posts/createpost.ts
+++ src/pages/api/posts/createpost.ts
@@ -4,25 +4,32 @@ import admin from "../../../utils/firebaseAdmin";
 
 export const config = {
   api: {
-    bodyParser: true, // Enable the body parser
-    externalResolver: true, // Optional, only if you have external resolvers
+    bodyParser: true,
+    externalResolver: true,
   },
 };
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  console.log("Received request at createpost API");
+
   const { db } = await connectToDatabase();
+  console.log("Connected to the database");
 
   if (req.method === "POST") {
     const authToken = req.headers.authorization?.split("Bearer ")[1];
 
     if (!authToken) {
+      console.log("No auth token provided");
       return res.status(401).json({ error: "Unauthorized" });
     }
 
     try {
       const decodedToken = await admin.auth().verifyIdToken(authToken);
+      console.log("Token verified successfully");
+
       const user = await admin
         .firestore()
         .collection("users")
@@ -30,11 +37,16 @@ export default async function handler(
         .get();
 
       if (!user.exists || user.data()?.role !== "admin") {
+        console.log("User does not have admin rights");
         return res.status(403).json({ error: "Forbidden" });
       }
 
       const newPost = req.body;
+      console.log("Inserting new post:", newPost);
+
       await db.collection("posts").insertOne(newPost);
+      console.log("Post inserted successfully");
+
       res.status(201).json(newPost);
     } catch (error) {
       console.error("Error verifying token or creating post:", error);
