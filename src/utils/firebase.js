125f3c1ff2723ff3272ebb5408906cd68ad8a3e4
Danielle Hunter <danielle.hunter@secturasoft.com>
2024-07-26 00:40:18 -0500

    remove debug code

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -16,6 +16,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-admin.firestore().settings({ logLevel: "debug" });
 
 export { auth, db };
