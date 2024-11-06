22567e3581399432dcdec23a66c151b678082b69
Zomievey <haylee_hunter@aol.com>
2024-11-06 00:23:14 -0600

    addind vercel analytics

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,15 @@
-
-import Footer from './Footer';
-import Navbar from './NavBar';
-
-import React, { ReactNode } from 'react';
+import { Analytics } from "@vercel/analytics/react";
+import Footer from "./Footer";
+import Navbar from "./NavBar";
+import React, { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className='flex flex-col min-h-screen'>
       <Navbar />
-      <main className="flex-grow">
-        {children}
-      </main>
+      <main className='flex-grow'>{children}</main>
       <Footer />
+      <Analytics />
     </div>
   );
 }
